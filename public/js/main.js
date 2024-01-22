95b58388248908efdb0f84f9e9a1f7d7788ada64
Laetitia Gallegos Lopez <laetitiagallegoslopez@MacBook-Air-de-Laetitia.local>
2024-01-22 07:39:59 +0100

    Comments useless deleted

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,3 @@
-// let randomPassWord = document.getElementById("inputuser")
-
-// function passwordGenerator() {
-//     let mixCharacters = "abcdefghijklmopqrstuvwxyz0123456789ABCDEFGHIJLKMNOLPQRSTUVWXYZ/*.?,)($*€#@&<>%ù;"
-//     let maxLengthPassword = 20
-//     let mdp
-
-//     for (let index = 0; index < maxLengthPassword; index++) {
-//         let Mixnumber = Math.floor(Math.random()*mixCharacters.length)
-//         mdp += mixCharacters.substring(Mixnumber,Mixnumber +1)
-//     }
-// }
-
 //-------Button clipboard------
 let btnClipboard = document.getElementById("")
 
